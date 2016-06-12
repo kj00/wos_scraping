f278749c0751053055f4eff95e70ec0c65230386
kj00 <kh03@outlook.com>
2016-06-12 15:49:17 +0900

    1.exclude all "and" "&" "or". 2.adjust all spaces 3.for search purpose, insert "and" in the middle spaces; because without inserting "and", order of words are preserved in WOS searching.

--- clearing_firmname.R
+++ clearing_firmname.R
@@ -15,17 +15,23 @@ osiris <- (osiris[, 2:4, with = F])
 firm <- cbind(osiris[[2]], osiris[[3]])
 
 #exclude signs except "&"
-firm <- gsub("[^[:alnum:][:space:]&]", " ", firm) 
+firm <- gsub("[^[:alnum:][:space:]]", " ", firm) 
 #make it capital
 firm <- toupper(firm) 
 
 #input words to exclude "." means one letter.
-remove1 <- c("INC", "LTD", "LIMITED", "AG", "A G", "COMPANY", "PLC","P L C", "CO", "CV", "C V", "S A B", "DE", "D E",
+remove1 <- c("INC", "LTD", "LIMITED", "AG", "A G", "COMPANY", "PLC","P L C", "CO", "CV", "C V", "S A B", "DE", "D E", "AND", "OR",
   "CORPORATION", "SA", "S A", "N V", "PARTNERSHIP", "GROUP", "CORP", "THE", "HOLDINGS","HOLDING", "STORE", "STORES", "ALLIANCE", "ALLIANCES") 
 
 
 #exclude words
 firm <- gsub(paste("\\<", remove1, "\\>", sep = "", collapse = "|"), "", firm)
 
-#exclude & at the end of string
-firm <- gsub("&[[:space:]]*$", "", firm)
+#exclude space at the end of string
+firm <- gsub("\\s+$", "", firm)
+
+#replace one or more spaces to one space
+firm <- gsub("\\s+", " ", firm)
+
+#for search, insert "and" in the space
+firm_and <- gsub(" ", " and ", firm)
